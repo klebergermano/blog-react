b038d1d6985db5c02efa60c77249585a06b1eb8d
Kleber Germano <kleber_germano@outlook.com>
2021-10-24 14:15:26 -0300

    Layout básico

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './assets/css/style.css';
+
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import Homepage from './pages/Home';
+import Categories from './pages/Categories';
+import About from './pages/About';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App" id='container'>
+        <aside id='bg_menu_lateral'>
+          <h1>Menu Lateral</h1>
+          <nav>
+            <ul>
+              <Link to='/'>Home</Link>
+              <Link to='/categories'>Categories</Link>
+              <Link to='/posts'>Posts</Link>
+              <Link to='/contact'>Contact</Link>
+            </ul>
+          </nav>
+        </aside>
+        <div id='content'>
+          <header className="App-header">
+          </header>
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/categories">
+              <Categories />
+            </Route>
+          </Switch>
+          <footer>
+Footer
+</footer>
+        </div>
+
+
+  
+      </div>
+    </Router>
   );
 }
 
