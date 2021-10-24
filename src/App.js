152adb84757bc4d2a0d69e2df49ce4359f31bd75
Kleber Germano <kleber_germano@outlook.com>
2021-10-24 20:13:20 -0300

    Author Avatar

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@
 import './assets/css/style.css';
 
 import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
-import Homepage from './pages/Home';
+import MainNavAside from './_includes/MainNavAside';
+import Homepage from './pages/Homepage';
 import Categories from './pages/Categories';
 import About from './pages/About';
 function App() {
   return (
     <Router>
       <div className="App" id='container'>
-        <aside id='bg_menu_lateral'>
-          <h1>Menu Lateral</h1>
-          <nav>
-            <ul>
-              <Link to='/'>Home</Link>
-              <Link to='/categories'>Categories</Link>
-              <Link to='/posts'>Posts</Link>
-              <Link to='/contact'>Contact</Link>
-            </ul>
-          </nav>
-        </aside>
+      <MainNavAside/>
         <div id='content'>
           <header className="App-header">
           </header>
